0890c93eb30b44972683f43708bd36784ec32725
Anubhav P <anubhav.p@echidnainc.com>
2022-01-07 17:03:45 +0530

    Added optional icon option in the CTA

--- react/CategoryTile.js
+++ react/CategoryTile.js
@@ -1,11 +1,14 @@
 import React from "react";
 import styles from "./index.css";
+import { Button } from 'vtex.styleguide'
+import { Icon } from 'vtex.store-icons'
 
 
 const CategoryTile = (props) => {
     const categoryName = props.categoryName;
     const categoryUrl = props.categoryUrl;
     const imgUrl = props.imgUrl;
+    const btnIcon = props.btnIconClass;
 
     return (
         <div className={styles.catContainer}>
@@ -14,8 +17,9 @@ const CategoryTile = (props) => {
                 alt={categoryName + " Banner Image"}
                 className={styles.imgSize}
             />
-            <a href={categoryUrl} title={categoryName} className={styles.imgName}>
+            <a href={categoryUrl} title={categoryName} className={styles.imgName + " bg-action-primary c-on-action-primary"} >
                 {categoryName}
+                {btnIcon && <span className={styles.icon}><Icon id={btnIcon} /></span>}
             </a>
         </div>
     );
